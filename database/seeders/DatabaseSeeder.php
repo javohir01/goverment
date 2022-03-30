7e4978aacff31615dfcceeb6566f09a41f662dfb
unknown <ruzikulovjavokhir005@mail.com>
2022-03-30 22:41:03 +0500

    fixed seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
         $this->call(RolesTableSeeder::class);
-        $this->call(RegionsTableSeeder::class);
-        $this->call(DistrictsTableSeeder::class);
+//        $this->call(RegionsTableSeeder::class);
+//        $this->call(DistrictsTableSeeder::class);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,12 +15,12 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $users = [
-            ['id' => '1','login' => 'admin1', 'role_id' => '1','email' => 'admin1@gmail.com','password' => bcrypt('123456')],
-            ['id' => '2','login' => 'admin2', 'role_id' => '1','email' => 'admin2@gmail.com','password' => bcrypt('123456')],
-            ['id' => '3','login' => 'reg_gov1', 'role_id' => '2','email' => 'reg_gov1@gmail.com','password' => bcrypt('123456')],
-            ['id' => '4','login' => 'reg_gov2', 'role_id' => '2','email' => 'reg_gov2@gmail.com','password' => bcrypt('123456')],
-            ['id' => '5','login' => 'dist_gov1', 'role_id' => '3','email' => 'dist_gov1@gmail.com','password' => bcrypt('123456')],
-            ['id' => '6','login' => 'dist_gov2', 'role_id' => '3','email' => 'dist_gov2@gmail.com','password' => bcrypt('123456')],
+            ['id' => '0','login' => 'admin1', 'role_id' => '1','email' => '1admin@gmail.com','password' => bcrypt('123456')],
+            ['id' => '1','login' => 'admin2', 'role_id' => '1','email' => '2admin@gmail.com','password' => bcrypt('123456')],
+            ['id' => '2','login' => 'reg_gov1', 'role_id' => '2','email' => 'reg_gov1@gmail.com','password' => bcrypt('123456')],
+            ['id' => '3','login' => 'reg_gov2', 'role_id' => '2','email' => 'reg_gov2@gmail.com','password' => bcrypt('123456')],
+            ['id' => '4','login' => 'dist_gov1', 'role_id' => '3','email' => 'dist_gov1@gmail.com','password' => bcrypt('123456')],
+            ['id' => '5','login' => 'dist_gov2', 'role_id' => '3','email' => 'dist_gov2@gmail.com','password' => bcrypt('123456')],
         ];
         DB::table('users')->insert($users);
     }
